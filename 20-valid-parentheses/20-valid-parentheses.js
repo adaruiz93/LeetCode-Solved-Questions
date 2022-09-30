7ba83150b09180a8357470ffce0c4928712afb90
Ada Ruiz <87149965+adaruiz93@users.noreply.github.com>
2022-09-29 22:42:55 -0700

    Time: 110 ms (35.18%), Space: 42.8 MB (40.51%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    if (s.length % 2 !== 0 ) return false;
+    let str = s.trim().split('');
+    let temp = [];
+    for(let i of str){
+        if(i == '(' || i=='{' || i=='['){
+            temp.push(i);
+        }
+        if(i==')'){
+            if(temp.pop() != '(') return false;
+        }
+        if(i=='}'){
+            if(temp.pop() != '{') return false;
+        }
+        if(i==']'){
+            if(temp.pop() != '[') return false;
+        }
+    };
+    return temp.length > 0 ? false: true;
+};
