97a85c9166ce7b2488eac21d0c7351a66d870c3c
Ada Ruiz <87149965+adaruiz93@users.noreply.github.com>
2022-10-01 20:30:54 -0700

    Time: 146 ms (33.46%), Space: 44.2 MB (89.43%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    const set = new Set(nums);
+    let i = 0;
+    set.forEach(num => {
+      nums[i] = num
+      i++
+    })
+
+    return set.size;
+};
