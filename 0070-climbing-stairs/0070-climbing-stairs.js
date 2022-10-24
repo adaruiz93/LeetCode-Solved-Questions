a50188934aedebb99ab4938fd8212841e855518c
Ada Ruiz <87149965+adaruiz93@users.noreply.github.com>
2022-10-23 20:32:57 -0700

    Time: 58 ms (96.69%), Space: 42 MB (41.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+let ans = [1,2];
+    
+for(let i = 2; i <n ; i++){
+    ans[i] = ans[i-1] + ans[i-2];
+    }
+    return ans[n-1];
+};
